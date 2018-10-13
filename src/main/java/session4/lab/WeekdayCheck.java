b8b145d6c7844274f94977bcbebbc901978a845b
tomingerson <thomas.bache@web.de>
2018-10-13 10:56:38 +0200

    added refactoring of Weekday class

--- src/main/java/session4/lab/WeekdayCheck.java
+++ src/main/java/session4/lab/WeekdayCheck.java
@@ -37,33 +37,6 @@ class WeekdayCheck {
     }
 }
 
-class Weekday {
-    public static final Weekday MONDAY = new Weekday("MONDAY", 1);
-    public static final Weekday TUESDAY = new Weekday("TUESDAY", 2);
-    public static final Weekday WEDNESDAY = new Weekday("WEDNESDAY", 3);
-    public static final Weekday THURSDAY = new Weekday("THURSDAY", 4);
-    public static final Weekday FRIDAY = new Weekday("FRIDAY", 5);
-    public static final Weekday SATURDAY = new Weekday("SATURDAY", 6);
-    public static final Weekday SUNDAY = new Weekday("SUNDAY", 0);
-
-    private String name;
-    private int index;
-
-    private Weekday(String name, int index) {
-        this.name = name;
-        this.index = index;
-    }
-
-    String name() {
-        return this.name;
-    }
-
-    int ordinal() {
-        return this.index;
-    }
-
-    static Weekday[] values() {
-        return new Weekday[]{SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY,
-                SATURDAY};
-    }
+enum Weekday {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
 }
