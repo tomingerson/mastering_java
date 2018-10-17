5fefbf4ab1b313228266177a83189a842d960664
Jan-Philipp Rathje <jpr@ergovia.de>
2018-10-17 19:15:06 +0200

    code for session 7

---
+++ src/main/java/session7/Equals.java
@@ -0,0 +1,56 @@
+package session7;
+
+import java.util.Objects;
+
+/**
+ * @author Created by ergouser on 16.10.18.
+ */
+public class Equals {
+
+    public static void main(String[] args) {
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(1, 2);
+
+        System.out.println(p1 == p2);
+        System.out.println(p1.equals(p2));
+
+        p2 = p1;
+
+        System.out.println(p1 == p2);
+        System.out.println(p1.equals(p2));
+
+        p2 = new Point(1, 3);
+
+        System.out.println(p1 == p2);
+        System.out.println(p1.equals(p2));
+
+        p2.y = 2;
+
+        System.out.println(p1 == p2);
+        System.out.println(p1.equals(p2));
+    }
+
+    static class Point {
+        private double x;
+        private double y;
+
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Point point = (Point) o;
+            return Double.compare(point.x, x) == 0 &&
+                    Double.compare(point.y, y) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
+}
---
+++ src/main/java/session7/StackOverflow.java
@@ -0,0 +1,15 @@
+package session7;
+
+/**
+ * @author Created by ergouser on 17.10.18.
+ */
+public class StackOverflow {
+
+    public static void main(String[] args) {
+        calculateFactorial(2);
+    }
+
+    private static int calculateFactorial(int number) {
+        return number * calculateFactorial(number - 1);
+    }
+}
