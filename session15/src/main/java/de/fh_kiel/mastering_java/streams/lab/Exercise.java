417616f2a3062777cc49d06d959173c1bbd6fc81
Jan-Philipp Rathje <jpr@ergovia.de>
2018-12-03 17:52:04 +0100

    Solution for Session15 Streams

--- session15/src/main/java/de/fh_kiel/mastering_java/streams/lab/Exercise.java
+++ session15/src/main/java/de/fh_kiel/mastering_java/streams/lab/Exercise.java
@@ -1,8 +1,7 @@
 package de.fh_kiel.mastering_java.streams.lab;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author Created by tom on 25.11.2018.
@@ -13,26 +12,26 @@ class Exercise {
     }
 
     static void printlnAllPersons(Collection<Person> persons) {
-        throw new UnsupportedOperationException("Not implemented");
+        persons.stream().forEach(System.out::println);
     }
 
     static void printlnAllPersonsSortedByLastname(Collection<Person> persons) {
-        throw new UnsupportedOperationException("Not implemented");
+        persons.stream().sorted(Comparator.comparing(Person::getLastname)).forEachOrdered(System.out::println);
     }
 
     static List<String> getAllPersonsLastnamesSortedByLastnameWithoutDuplicates(Collection<Person> persons) {
-        throw new UnsupportedOperationException("Not implemented");
+        return persons.stream().map(Person::getLastname).distinct().sorted().collect(Collectors.toList());
     }
 
     static boolean areAllPetersFromGermany(Collection<Person> persons) {
-        throw new UnsupportedOperationException("Not implemented");
+        return persons.stream().filter(p -> Objects.equals("Peter", p.getFirstname())).allMatch(p -> Objects.equals(Country.GERMANY, p.getNationality()));
     }
 
     static List<String> getAllPersonsResidencesSortedWithoutDuplicates(Collection<Person> persons) {
-        throw new UnsupportedOperationException("Not implemented");
+        return persons.stream().map(Person::getResidency).distinct().sorted().collect(Collectors.toList());
     }
 
     static Optional<Person> findPeterTallerThan(Collection<Person> persons, int height) {
-        throw new UnsupportedOperationException("Not implemented");
+        return persons.stream().filter(p -> Objects.equals("Peter", p.getFirstname())).filter(person -> height < person.getHeight()).findAny();
     }
 }
