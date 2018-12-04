da72ab29a77c89606b4c04e9212c4eaf3ebe30ea
Jan-Philipp Rathje <jpr@ergovia.de>
2018-12-05 00:12:16 +0100

    Solution for Session16 Streams

--- session16/src/main/java/de/fh_kiel/mastering_java/streams/lab/Exercise.java
+++ session16/src/main/java/de/fh_kiel/mastering_java/streams/lab/Exercise.java
@@ -1,10 +1,8 @@
 package de.fh_kiel.mastering_java.streams.lab;
 
-import java.util.DoubleSummaryStatistics;
-import java.util.List;
-import java.util.Map;
-import java.util.OptionalDouble;
-import java.util.OptionalInt;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Exercise for session 16: Streams II
@@ -15,41 +13,80 @@ class Exercise {
 
     static OptionalInt getHeightOfPersonInTheMiddle(List<Person> persons) {
         // not possible in one line (?)
-        throw new UnsupportedOperationException("Not yet implemented");
+        int size = persons.size();
+        return persons.stream()
+                .mapToInt(Person::getHeight)
+                .sorted()
+                .skip(size / 2L)
+                .findFirst();
     }
 
     static OptionalDouble getAverageHeightOfGermanPersons(List<Person> persons) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return persons.stream()
+                .mapToInt(Person::getHeight)
+                .average();
     }
 
     static List<String> getAllUniqueCitynamesAnyPersonEverLivedIn(List<Person> persons) {
         // use flatMap
-        throw new UnsupportedOperationException("Not yet implemented");
+        return persons.stream()
+                .flatMap(person -> person.getCitiesLivedIn().stream())
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     static DoubleSummaryStatistics getSummaryStatisticsOnPersonsWeights(List<Person> persons) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return persons.stream()
+                .mapToDouble(Person::getWeight)
+                .summaryStatistics();
     }
 
     static List<Integer> generatePowersOf2Between16And256() {
         // use iterate to create the stream
-        throw new UnsupportedOperationException("Not yet implemented");
+        return Stream.iterate(2, i -> 2 * i)
+                .skip(3L)
+                .limit(5L)
+                .collect(Collectors.toList());
     }
 
     static Map<Boolean, List<Integer>> partitionListByEvenAndOdd(List<Integer> numbers) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return numbers.stream()
+                .filter(Objects::nonNull)
+                .collect(
+                        Collectors.partitioningBy(i -> i % 2 == 0)
+                );
     }
 
     static Map<Character, List<Person>> groupPersonsByFirstLetterOfLastname(List<Person> persons) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return persons.stream()
+                .collect(
+                        Collectors.groupingBy(person -> person.getLastname().charAt(0))
+                );
     }
 
     static Map<Character, List<Gender>> groupPersonsgenderByFirstLetterOfLastname(List<Person> persons) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return persons.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                person -> person.getLastname().charAt(0),
+                                Collectors.mapping(
+                                        Person::getGender,
+                                        Collectors.toList()
+                                )
+                        )
+                );
     }
 
     static List<Person> allPersonsWithTheSameLastname(List<Person> persons) {
         // use groupingBy Lastname and stream the result
-        throw new UnsupportedOperationException("Not yet implemented");
+        return persons.stream()
+                .collect(
+                        Collectors.groupingBy(Person::getLastname, Collectors.toList())
+                )
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().size() > 1)
+                .flatMap(entry -> entry.getValue().stream())
+                .collect(Collectors.toList());
     }
 }
