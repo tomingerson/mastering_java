0161ae39c88826695553992b28fef5775d23e87c
Jan-Philipp Rathje <jpr@ergovia.de>
2018-12-16 13:01:33 +0100

    Dining Philosophers using fair Semaphores

--- session17/src/main/java/de/fh_kiel/mastering_java/concurrency/lab/Fork.java
+++ session17/src/main/java/de/fh_kiel/mastering_java/concurrency/lab/Fork.java
@@ -3,13 +3,13 @@ package de.fh_kiel.mastering_java.concurrency.lab;
 import java.util.concurrent.Semaphore;
 
 /**
- * A simple class without any methods or members representing a fork for a philosopher
- * to take.
+ * A simple class without any methods or members representing a fork for a philosopher to take.
+ *
  * @author Created by tom on 09.12.2018.
  */
 class Fork extends Semaphore {
 
-    public Fork() {
+    Fork() {
         super(1, true);
     }
 }
